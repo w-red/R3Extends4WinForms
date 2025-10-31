ccba036bb3c5d22369e7a2adb9923ea1aca0d9c2
terashi <yterashi@gmail.com>
2025-10-31 15:19:41 +0900

    add [Numeric/Domain]UpDown.

---
+++ R3Extends4DomainUpDown.cs
@@ -0,0 +1,27 @@
+﻿using R3;
+
+namespace R3Extends4WinForms
+{
+    /// <summary><see cref="R3"/>'s Extends for <see cref="DomainUpDown"/>.</summary>
+    /// <remarks>
+    /// Add extension methods for any events you need.<br />
+    /// Maybe there are some things that have been expanded by your Framework,<br />
+    /// so they need to be added.
+    /// The type must follow the type of each event handler.
+    /// </remarks>
+    public static class R3Extends4DomainUpDown
+    {
+        /// <summary><see cref="DomainUpDown.SelectedItemChanged"/> as <see cref="Observable(Of EventArgs)"/></summary>
+        /// <param name="dud">target</param>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public static Observable<EventArgs> SelectedItemChangedAsObservable(
+            this DomainUpDown dud,
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => dud.SelectedItemChanged += h,
+                h => dud.SelectedItemChanged -= h,
+                tkn);
+    }
+}
