0cc8bcc87bc61cc94ce63f7662a3d14a801c7519
terashi <yterashi@gmail.com>
2025-10-31 14:05:59 +0900

    add R3ExtendsScrollableControl.

---
+++ R3ExtendsScrollableControl.cs
@@ -0,0 +1,27 @@
+﻿using R3;
+
+namespace R3Extends4WinForms
+{
+    /// <summary><see cref="R3"/>'s Extends for <see cref="ScrollableControl"/>.</summary>
+    /// <remarks>
+    /// Add extension methods for any events you need.<br />
+    /// Maybe there are some things that have been expanded by your Framework,<br />
+    /// so they need to be added.
+    /// The type must follow the type of each event handler.
+    /// </remarks>
+    public static class R3ExtendsScrollableControl
+    {
+        /// <summary><see cref="ScrollableControl.Scroll"/> as <see cref="Observable(Of EventArgs)"/></summary>
+        /// <param name="ctrl">target</param>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public static Observable<ScrollEventArgs> MoveAsObservable(
+            this ScrollableControl ctrl,
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<ScrollEventHandler, ScrollEventArgs>(
+                h => (s, e) => h(e),
+                h => ctrl.Scroll += h,
+                h => ctrl.Scroll -= h,
+                tkn);
+    }
+}
