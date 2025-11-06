352d617658d5e824f94adf9dee38b4ee68bb781c
terashi <yterashi@gmail.com>
2025-11-06 13:01:37 +0900

    add StatusStrip extends.

---
+++ StatusStripR3Extends.cs
@@ -0,0 +1,27 @@
+﻿using R3;
+
+namespace R3Extends4WinForms
+{
+    /// <summary><see cref="R3"/>'s Extends for <see cref="StatusStrip"/>.</summary>
+    /// <remarks>
+    /// Add extension methods for any events you need.<br />
+    /// Maybe there are some things that have been expanded by your Framework,<br />
+    /// so they need to be added.
+    /// The type must follow the type of each event handler.
+    /// </remarks>
+    public static class StatusStripR3Extends
+    {
+        /// <summary><see cref="StatusStrip.PaddingChanged"/> as <see cref="Observable(Of EventArgs)"/></summary>
+        /// <param name="ss">target</param>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public static Observable<EventArgs> PaddingChangedAsObservable(
+            this StatusStrip ss,
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => ss.PaddingChanged += h,
+                h => ss.PaddingChanged -= h,
+                tkn);
+    }
+}
