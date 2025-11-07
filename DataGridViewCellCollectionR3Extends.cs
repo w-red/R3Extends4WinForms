3cebd5dc052676f788bf911a5ffd22e8e2ccf50b
terashi <yterashi@gmail.com>
2025-11-07 10:54:52 +0900

    add DataGridViewCellCollection extends.

---
+++ DataGridViewCellCollectionR3Extends.cs
@@ -0,0 +1,28 @@
+﻿using R3;
+using System.ComponentModel;
+
+namespace R3Extends4WinForms
+{
+    /// <summary><see cref="R3"/>'s Extends for <see cref="DataGridViewCellCollection"/>.</summary>
+    /// <remarks>
+    /// Add extension methods for any events you need.<br />
+    /// Maybe there are some things that have been expanded by your Framework,<br />
+    /// so they need to be added.
+    /// The type must follow the type of each event handler.
+    /// </remarks>
+    public static class DataGridViewCellCollectionR3Extends
+    {
+        /// <summary><see cref="DataGridViewCellCollection.CollectionChanged"/> as <see cref="Observable(Of EventArgs)"/></summary>
+        /// <param name="dgvcc">target</param>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public static Observable<CollectionChangeEventArgs> CollectionChangedAsObservable(
+            this DataGridViewCellCollection dgvcc,
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<CollectionChangeEventHandler, CollectionChangeEventArgs>(
+                h => (s, e) => h(e),
+                h => dgvcc.CollectionChanged += h,
+                h => dgvcc.CollectionChanged -= h,
+                tkn);
+    }
+}
