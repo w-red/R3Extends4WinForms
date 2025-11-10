c22b4c249b8e37c970d91ed7c1735bd74bd28b62
terashi <yterashi@gmail.com>
2025-11-10 14:01:54 +0900

    add TaskDialogVerificationCheckBox extends.

---
+++ TaskDialogVerificationCheckBoxR3Extends.cs
@@ -0,0 +1,27 @@
+﻿using R3;
+
+namespace R3Extends4WinForms
+{
+    /// <summary><see cref="R3"/>'s Extends for <see cref="TaskDialogVerificationCheckBox"/>.</summary>
+    /// <remarks>
+    /// Add extension methods for any events you need.<br />
+    /// Maybe there are some things that have been expanded by your Framework,<br />
+    /// so they need to be added.
+    /// The type must follow the type of each event handler.
+    /// </remarks>
+    public static class TaskDialogVerificationCheckBoxR3Extends
+    {
+        /// <summary><see cref="TaskDialogVerificationCheckBox.CheckedChanged"/> as <see cref="Observable"/></summary>
+        /// <param name="tdvcb">target</param>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public static Observable<EventArgs> CheckedChangedAsObservable(
+            this TaskDialogVerificationCheckBox tdvcb,
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => tdvcb.CheckedChanged += h,
+                h => tdvcb.CheckedChanged -= h,
+                tkn);
+    }
+}
