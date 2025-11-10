e5d0b1e2abc7e4df8e2869f68a83457222b43832
terashi <yterashi@gmail.com>
2025-11-10 14:45:14 +0900

    add WebBrowserBase extends.

---
+++ WebBrowserBaseR3Extends.cs
@@ -0,0 +1,16 @@
+﻿using R3;
+using System.ComponentModel;
+
+namespace R3Extends4WinForms
+{
+    /// <summary><see cref="R3"/>'s Extends for <see cref="WebBrowserBase"/>.</summary>
+    /// <remarks>
+    /// Add extension methods for any events you need.<br />
+    /// Maybe there are some things that have been expanded by your Framework,<br />
+    /// so they need to be added.
+    /// The type must follow the type of each event handler.
+    /// </remarks>
+    public static class WebBrowserBaseR3Extends
+    {
+    }
+}
