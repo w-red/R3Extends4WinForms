eb92f85d6f13ff61e29b453729f70fdfa1242ea0
terashi <yterashi@gmail.com>
2025-11-10 12:09:12 +0900

    add SaveFileDialog extends.

---
+++ SaveFileDialogR3Extends.cs
@@ -0,0 +1,15 @@
+﻿using R3;
+
+namespace R3Extends4WinForms
+{
+    /// <summary><see cref="R3"/>'s Extends for <see cref="SaveFileDialog"/>.</summary>
+    /// <remarks>
+    /// Add extension methods for any events you need.<br />
+    /// Maybe there are some things that have been expanded by your Framework,<br />
+    /// so they need to be added.
+    /// The type must follow the type of each event handler.
+    /// </remarks>
+    public static class SaveFileDialogR3Extends
+    {
+    }
+}
