77a69f6755b215863c7e0d4979d478017f014e2b
terashi <yterashi@gmail.com>
2025-11-10 14:27:35 +0900

    add ToolStripSeparator extends.

---
+++ ToolStripSeparatorR3Extends.cs
@@ -0,0 +1,15 @@
+﻿using R3;
+
+namespace R3Extends4WinForms
+{
+    /// <summary><see cref="R3"/>'s Extends for <see cref="ToolStripSeparator"/>.</summary>
+    /// <remarks>
+    /// Add extension methods for any events you need.<br />
+    /// Maybe there are some things that have been expanded by your Framework,<br />
+    /// so they need to be added.
+    /// The type must follow the type of each event handler.
+    /// </remarks>
+    public static class ToolStripSeparatorR3Extends
+    {
+    }
+}
