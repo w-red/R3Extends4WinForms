d1ac3c2f5013ccf5953e5162c9ae0fdedd5c6f82
terashi <yterashi@gmail.com>
2025-11-19 09:58:07 +0900

    add test.

--- tests/R3Extends4WinFormsTest/AsObservableTest.cs
+++ tests/R3Extends4WinFormsTest/AsObservableTest.cs
@@ -1,13 +1,25 @@
 using Shouldly;
+using System.Windows.Forms;
+using R3Extends4WinForms;
 
-namespace R3Extends4WinFormsTest
+namespace R3Extends4WinFormsTest;
+
+public class AsObservableTest
 {
-    public class AsObservableTest
+    [Fact]
+    public void FormLoadAsObservableTest()
     {
-        [Fact]
-        public void Test1()
-        {
+        var form = new Form();
+        var observed = false;
+        var observable = 
+            form.
+            LoadAsObservable();
+        using var subscription =
+            observable.
+            Subscribe(_ => { observed = true; return Disposable.Empty; });
 
-        }
+        form.Show();
+        observed.ShouldBeTrue();
+        form.Close();
     }
 }
