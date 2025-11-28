b7596fe530bbcb648dc48a7ec1ba85eef3490906
terashi <yterashi@gmail.com>
2025-11-28 14:53:05 +0900

    extention block applied.

--- src/ButtonBaseR3Extends.cs
+++ src/ButtonBaseR3Extends.cs
@@ -11,57 +11,56 @@ namespace R3Extends4WinForms;
 /// </remarks>
 public static class ButtonBaseR3Extends
 {
-    /// <summary><see cref="ButtonBase.AutoSizeChanged"/> as <see cref="Observable"/></summary>
-    /// <param name="bb">target</param>
-    /// <param name="tkn">token</param>
-    /// <returns>Observable object.</returns>
-    public static Observable<EventArgs> AutoSizeChangedAsObservable(
-        this ButtonBase bb,
-        CancellationToken tkn = default) =>
-        Observable.FromEvent<EventHandler, EventArgs>(
-            h => (s, e) => h(e),
-            h => bb.AutoSizeChanged += h,
-            h => bb.AutoSizeChanged -= h,
-            tkn);
+    /// <summary>Extensions for <see cref="ButtonBase"/>.</summary>
+    extension(ButtonBase bb)
+    {
+        /// <summary><see cref="ButtonBase.AutoSizeChanged"/> as <see cref="Observable"/></summary>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public Observable<EventArgs> AutoSizeChangedAsObservable(
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => bb.AutoSizeChanged += h,
+                h => bb.AutoSizeChanged -= h,
+                tkn);
 
 #if NET7_0_OR_GREATER
-    /// <summary><see cref="ButtonBase.CommandCanExecuteChanged"/> as <see cref="Observable"/></summary>
-    /// <param name="bb">target</param>
-    /// <param name="tkn">token</param>
-    /// <returns>Observable object.</returns>
-    public static Observable<EventArgs> CommandCanExecuteChangedAsObservable(
-        this ButtonBase bb,
-        CancellationToken tkn = default) =>
-        Observable.FromEvent<EventHandler, EventArgs>(
-            h => (s, e) => h(e),
-            h => bb.CommandCanExecuteChanged += h,
-            h => bb.CommandCanExecuteChanged -= h,
-            tkn);
+        /// <summary><see cref="ButtonBase.CommandCanExecuteChanged"/> as <see cref="Observable"/></summary>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public Observable<EventArgs> CommandCanExecuteChangedAsObservable(
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => bb.CommandCanExecuteChanged += h,
+                h => bb.CommandCanExecuteChanged -= h,
+                tkn);
 
-    /// <summary><see cref="ButtonBase.CommandChanged"/> as <see cref="Observable"/></summary>
-    /// <param name="bb">target</param>
-    /// <param name="tkn">token</param>
-    /// <returns>Observable object.</returns>
-    public static Observable<EventArgs> CommandChangedAsObservable(
-        this ButtonBase bb,
-        CancellationToken tkn = default) =>
-        Observable.FromEvent<EventHandler, EventArgs>(
-            h => (s, e) => h(e),
-            h => bb.CommandChanged += h,
-            h => bb.CommandChanged -= h,
-            tkn);
+        /// <summary><see cref="ButtonBase.CommandChanged"/> as <see cref="Observable"/></summary>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public Observable<EventArgs> CommandChangedAsObservable(
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => bb.CommandChanged += h,
+                h => bb.CommandChanged -= h,
+                tkn);
 
-    /// <summary><see cref="ButtonBase.CommandParameterChanged"/> as <see cref="Observable"/></summary>
-    /// <param name="bb">target</param>
-    /// <param name="tkn">token</param>
-    /// <returns>Observable object.</returns>
-    public static Observable<EventArgs> CommandParameterChangedAsObservable(
-        this ButtonBase bb,
-        CancellationToken tkn = default) =>
-        Observable.FromEvent<EventHandler, EventArgs>(
-            h => (s, e) => h(e),
-            h => bb.CommandParameterChanged += h,
-            h => bb.CommandParameterChanged -= h,
-            tkn);
+        /// <summary><see cref="ButtonBase.CommandParameterChanged"/> as <see cref="Observable"/></summary>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public Observable<EventArgs> CommandParameterChangedAsObservable(
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => bb.CommandParameterChanged += h,
+                h => bb.CommandParameterChanged -= h,
+                tkn);
 #endif
+
+    }
+
+
 }
