a16d503111b5e3691a5db8e07c6b35f39d4b8719
terashi <yterashi@gmail.com>
2025-11-28 12:05:21 +0900

    suppress CS1574 warning.

--- src/BindableComponentR3Extends.cs
+++ src/BindableComponentR3Extends.cs
@@ -4,6 +4,9 @@ using System.Windows.Forms;
 namespace R3Extends4WinForms;
 
 #if NET7_0_OR_GREATER
+#else
+#pragma warning disable CS1574 // XML comment has cref attribute 'BindableComponent' that could not be resolved
+#endif
 /// <summary><see cref="R3"/>'s Extends for <see cref="BindableComponent"/>.</summary>
 /// <remarks>
 /// Add extension methods for any events you need.<br />
@@ -11,19 +14,25 @@ namespace R3Extends4WinForms;
 /// so they need to be added.
 /// The type must follow the type of each event handler.
 /// </remarks>
+#if NET7_0_OR_GREATER
+#else
+#pragma warning disable CS1574 // XML comment has cref attribute 'BindableComponent' that could not be resolved
+#endif
 public static class BindableComponentR3Extends
 {
-    /// <summary><see cref="BindableComponent.BindingContextChanged"/> as <see cref="Observable"/></summary>
-    /// <param name="bc">target</param>
-    /// <param name="tkn">token</param>
-    /// <returns>Observable object.</returns>
-    public static Observable<EventArgs> BindingContextChangedAsObservable(
-        this BindableComponent bc,
-        CancellationToken tkn = default) =>
-        Observable.FromEvent<EventHandler, EventArgs>(
-            h => (s, e) => h(e),
-            h => bc.BindingContextChanged += h,
-            h => bc.BindingContextChanged -= h,
-            tkn);
-}
+#if NET7_0_OR_GREATER
+    extension(BindableComponent bc)
+    {
+        /// <summary><see cref="BindableComponent.BindingContextChanged"/> as <see cref="Observable"/></summary>
+        /// <param name="tkn">token</param>
+        /// <returns>Observable object.</returns>
+        public Observable<EventArgs> BindingContextChangedAsObservable(
+            CancellationToken tkn = default) =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => bc.BindingContextChanged += h,
+                h => bc.BindingContextChanged -= h,
+                tkn);
+    }
 #endif
+}
