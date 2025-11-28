e13463342c9cd047585e9b23ff62fab31f235d39
terashi <yterashi@gmail.com>
2025-11-28 11:07:07 +0900

    extention block applied.

--- src/ApplicationContextR3Extends.cs
+++ src/ApplicationContextR3Extends.cs
@@ -1,22 +1,23 @@
 ﻿using R3;
 
-namespace R3Extends4WinForms
+namespace R3Extends4WinForms;
+
+/// <summary><see cref="R3"/>'s Extends for <see cref="ApplicationContext"/>.</summary>
+/// <remarks>
+/// Add extension methods for any events you need.<br />
+/// Maybe there are some things that have been expanded by your Framework,<br />
+/// so they need to be added.
+/// The type must follow the type of each event handler.
+/// </remarks>
+public static class ApplicationContextR3Extends
 {
-    /// <summary><see cref="R3"/>'s Extends for <see cref="ApplicationContext"/>.</summary>
-    /// <remarks>
-    /// Add extension methods for any events you need.<br />
-    /// Maybe there are some things that have been expanded by your Framework,<br />
-    /// so they need to be added.
-    /// The type must follow the type of each event handler.
-    /// </remarks>
-    public static class ApplicationContextR3Extends
+    /// <summary>Extensions for <see cref="ApplicationContext"/>.</summary>
+    extension(ApplicationContext ac)
     {
         /// <summary><see cref="ApplicationContext.ThreadExit"/> as <see cref="Observable"/></summary>
-        /// <param name="ac">target</param>
         /// <param name="tkn">token</param>
         /// <returns>Observable object.</returns>
-        public static Observable<EventArgs> ThreadExitAsObservable(
-            this ApplicationContext ac,
+        public Observable<EventArgs> ThreadExitAsObservable(
             CancellationToken tkn = default) =>
             Observable.FromEvent<EventHandler, EventArgs>(
                 h => (s, e) => h(e),
