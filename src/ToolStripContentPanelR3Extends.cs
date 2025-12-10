a901908c7ce5c6411db60697a8d3fe41fe433cd1
terashi <yterashi@gmail.com>
2025-12-10 11:32:47 +0900

    fix #3 ToolStripContentPanel is enabled at NETCOREAPP3_1_OR_GREATER.

--- src/ToolStripContentPanelR3Extends.cs
+++ src/ToolStripContentPanelR3Extends.cs
@@ -2,9 +2,6 @@
 
 namespace R3Extends4WinForms;
 
-#if NETCOREAPP3_1_OR_GREATER
-#pragma warning disable CS1574 // XML comment has cref attribute that could not be resolved
-#endif
 /// <summary><see cref="R3"/>'s Extends for <see cref="ToolStripContentPanel"/>.</summary>
 /// <remarks>
 /// Add extension methods for any events you need.<br />
@@ -12,12 +9,8 @@ namespace R3Extends4WinForms;
 /// so they need to be added.
 /// The type must follow the type of each event handler.
 /// </remarks>
-#if NETCOREAPP3_1_OR_GREATER
-#pragma warning restore CS1574 // XML comment has cref attribute that could not be resolved
-#endif
 public static class ToolStripContentPanelR3Extends
 {
-#if !NETCOREAPP3_1_OR_GREATER
     /// <summary><see cref="ToolStripContentPanel"/> extension methods.</summary>
     extension(ToolStripContentPanel tscp)
     {
@@ -43,5 +36,4 @@ public static class ToolStripContentPanelR3Extends
                 h => tscp.RendererChanged -= h,
                 tkn);
     }
-#endif
 }
